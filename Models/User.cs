6942e5c3743ed0694c70dea8c5b66fecc2352774
Zhuldyz <149614840+Assylbekovna@users.noreply.github.com>
2025-12-02 12:44:31 +0600

    Create User.cs

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+namespace OnlineStoreSequence.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        
+        public override string ToString()
+        {
+            return $"{Name} ({Email})";
+        }
+    }
+}
